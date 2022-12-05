35a177dac66311aaeace41f29d7530ae56781faf
torbenwb <torbenwbernhard@gmail.com>
2022-12-05 08:58:58 -0800

    new build

--- Build/MojaveRunnerConverence_WebGL.framework.js
+++ Build/MojaveRunnerConverence_WebGL.framework.js
@@ -1292,7 +1292,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2335296;
+STATICTOP = STATIC_BASE + 2335632;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3346,7 +3346,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2335296;
+var STATIC_BUMP = 2335632;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
